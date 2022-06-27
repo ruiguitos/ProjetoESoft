48922e2cb44379fd21820a4051321f209d411209
Gerson Fernandes <2200680@my.ipleiria.pt>
2022-06-27 03:21:56 +0100

    classes

---
+++ src/main/java/modelo/Marca.java
@@ -0,0 +1,13 @@
+package modelo;
+
+public class Marca {
+    private String nome;
+
+    public Marca(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
